b8fb3ef2a991bd6718ebdddeb053c8a780e9ae1d
gnaizer <gntnaizer@gmail.com>
2018-02-02 12:54:15 +0200

    push

---
+++ index.js
@@ -0,0 +1,22 @@
+// var a = +prompt('enter your number',' ');
+// var b = +prompt('enter your number',' ');
+// alert(a + b);
+
+// var price1 = 0.3, price2 = 0.4;
+// alert( +(price1 + price2).toFixed(2));
+
+// function getDecimal(num){
+//   return num - Math.floor(num);
+// }
+// alert( getDecimal(12.4934) );
+
+// var max = 20;
+// alert(Math.random()*max);
+
+// function() = randomInteger(min, max);
+//   min = 1;
+//   max = 50;
+// alert(min + randomInteger() + (max-min));
+
+// var git = "Hello world!";
+// alert(git.toUpperCase());
